a76a0ffaac7d0b7786c03309c63461d16c10bf20
Oussama Bezzad <2561446+obezzad@users.noreply.github.com>
2025-05-20 00:12:16 +0400

    Refactor Home component to streamline data fetching and improve status display
    - Removed unused authStore initialization and related effects.
    - Consolidated status management by utilizing the new useStatus hook.
    - Enhanced UI to show download and upload progress dynamically.
    - Cleaned up code by eliminating unnecessary variables and effects.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,20 +7,16 @@ import { usePowerSync } from '@powersync/react';
 import { useQuery, useStatus } from '@powersync/react';
 import { AbstractPowerSyncDatabase } from '@powersync/web';
 import store from '@/stores/RootStore';
-import { initializeAuthStore } from '@/stores/AuthStore';
 import { observer } from 'mobx-react-lite';
 import { v5 as uuidv5 } from 'uuid';
 import { userService } from '@/library/powersync/userService';
-import { measure, measureOnce, METRICS, registerLastSync, registerStart, timestamp } from '@/utils/metrics';
+import { measureOnce, METRICS, registerLastSync, registerStart, timestamp } from '@/utils/metrics';
 import { usePrevious } from '@/hooks/usePrevious';
-import { useHasFocus } from '@/hooks/useHasFocus';
 
 const Home = observer(() => {
   const db = usePowerSync();
 
   if (!db) throw new Error('PowerSync context not found');
-
-  const authStore = initializeAuthStore();
 
   const local_id = store.session?.user?.user_metadata?.local_id;
   const [nodeService] = useState(() => new NodeService(db as AbstractPowerSyncDatabase));
@@ -75,11 +71,8 @@ const Home = observer(() => {
     local_id
   ]);
   const { data: buckets } = useQuery(`SELECT count(DISTINCT bucket) as bucket_count FROM ps_oplog`);
-  const { data: downloadedOps } = useQuery('select count() as count from ps_oplog');
-  const prevOps = usePrevious(downloadedOps?.[0]?.count);
   const { data: pendingUpload } = useQuery('select count(distinct tx_id) as count from ps_crud');
-  const status = useStatus();
-  const isTabFocused = useHasFocus();
+  const { downloadProgress, dataFlowStatus, connected, hasSynced } = useStatus();
 
   const [remoteCount, setRemoteCount] = useState<number | null>(null);
 
@@ -90,33 +83,6 @@ const Home = observer(() => {
   useEffect(() => {
     registerLastSync();
   }, [nodes]);
-
-  useEffect(() => {
-    if (!prevCount)
-      return;
-
-    if (prevCount === remoteNodes?.[0]?.count && prevOps === downloadedOps?.[0]?.count)
-      return;
-
-    timestamp("PULL");
-
-    if (isTabFocused) {
-      measure("FOREGROUND");
-    }
-  }, [downloadedOps]);
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const { data, count } = await authStore.supabase
-        .from('nodes')
-        .select('id', { count: 'exact', head: true })
-        .eq('user_id', local_id);
-
-      setRemoteCount(count);
-    };
-
-    fetchData();
-  }, [downloadedOps]);
 
   useEffect(() => {
     if (!store.selectedNodeId) {
@@ -132,24 +98,35 @@ const Home = observer(() => {
     <main className="flex h-[calc(100vh-theme(spacing.16))]">
       <aside className="hidden sm:flex sm:w-72 py-1 px-2 border-r flex-col gap-0.5 text-xs">
         <div className="text-gray-600 leading-tight">User's buckets: <b className="text-black">{buckets[0]?.bucket_count ?? 0}</b></div>
-        <div className="text-gray-600 leading-tight">Synced nodes: <b className="text-black">
-          {allNodes[0]?.count ?? 0}
-          {remoteCount && <> / {remoteCount} ({Math.round((allNodes[0]?.count ?? 0) / remoteCount * 10000) / 100}%)</>}
-        </b></div>
         <div className="text-gray-600 leading-tight">User nodes: <b className="text-black">{userNodes[0]?.count ?? 0}</b></div>
-        <div className="text-gray-600 leading-tight">Downloaded ops: <b className="text-black">{downloadedOps[0]?.count ?? 0}</b></div>
-        <div className="text-gray-600 leading-tight">Mutations pending upload: <b className="text-black">{pendingUpload[0]?.count ?? 0}</b></div>
         <div className="text-gray-600 leading-tight">Selected ID: <b className="text-black truncate block">{store.selectedNodeId}</b></div>
         <div className="text-gray-600 leading-tight">Selected nodes count: <b className="text-black">{store._syncedNodes.length}</b></div>
         <div className="leading-tight">
-          {status.connected ?
+          {connected ?
             <span className='text-green-500'>Connected</span> :
             <span className='text-red-500'>Disconnected</span>}
           {' • '}
-          {status.hasSynced ?
+          {hasSynced ?
             <span className='text-green-500'>Initial sync done</span> :
             <span className='text-red-500'>Pending initial sync</span>}
         </div>
+        {dataFlowStatus.downloading && (
+          <>
+            <div className="text-blue-600 leading-tight">
+              <div>Downloading...</div>
+              <div>
+                {downloadProgress.downloadedOperations}
+                / {downloadProgress.totalOperations}
+                {` `} ({Math.round(downloadProgress.downloadedFraction * 10000) / 100}%)
+              </div>
+            </div>
+          </>
+        )}
+        {dataFlowStatus.uploading && <>
+          <div className="text-blue-600 leading-tight">
+            Uploading... ({pendingUpload[0]?.count ?? 0})
+          </div>
+        </>}
       </aside>
       <section className="flex-1 h-full overflow-y-auto">
         <TreeView
